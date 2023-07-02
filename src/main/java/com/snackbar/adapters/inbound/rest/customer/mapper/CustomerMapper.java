7886672becf6605b3c19a0c0bd9ab0c309371bb5
Gutemberg <gutembergrcc@gmail.com>
2023-07-01 21:30:21 -0300

    Refactore to put DTO pattern

--- src/main/java/com/snackbar/adapters/inbound/rest/customer/mapper/CustomerMapper.java
+++ src/main/java/com/snackbar/adapters/inbound/rest/customer/mapper/CustomerMapper.java
@@ -5,11 +5,6 @@ import com.snackbar.application.core.usecase.customer.CustomerOutput;
 
 public interface CustomerMapper {
 
-    static CustomerResponse toConsumerResponse(final CreateCustomerOutput createCustomerOutput) {
-        return new CustomerResponse(createCustomerOutput.id(),
-                createCustomerOutput.firstName(), createCustomerOutput.lastName(), createCustomerOutput.cpf());
-    }
-
     static CustomerResponse toConsumerResponse(final CustomerOutput customerOutput) {
         return new CustomerResponse(customerOutput.id(),
                 customerOutput.firstName(), customerOutput.lastName(), customerOutput.cpf());
